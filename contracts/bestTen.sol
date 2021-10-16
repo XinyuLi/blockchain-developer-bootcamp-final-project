4395310af2b74ea1c54fbe4514aa3e187b373396
Xinyu Li <candycis.cpp@gmail.com>
2021-10-16 15:06:20 -0700

    initial scaffold

---
+++ contracts/bestTen.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract bestTen {
+  constructor() public {
+  }
+  function registerCustomer(address _writer) {
+
+	// registers customer
+
+  };
+  function registerVote(uint restrauntID) {
+
+	// registers the vote of the voter
+
+  };
+}
